56d428f1ea7e459b967b7c013900a41797eac320
qazi0aimal <139748115+qazi0aimal@users.noreply.github.com>
2023-08-18 14:54:34 +0500

    adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit {
+  itemForm!: FormGroup;
+
+  items: any[] = [];
+
+  isActive: any[] = [];
+
+  itemIndex: any = null;
+
+  ngOnInit(): void {
+    this.itemForm = new FormGroup({
+      item: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onItem() {
+    let itemData = this.itemForm.getRawValue();
+    if (this.itemIndex !== null) {
+      this.items[this.itemIndex].item = this.itemForm.value.item;
+      this.itemIndex = null;
+    } else {
+      this.items.push(itemData);
+    }
+    this.itemForm.reset();
+  }
+
+  del(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  toggle(index: number) {
+    this.isActive[index] = !this.isActive[index];
+  }
+
+  edit(index: number) {
+    this.itemIndex = index;
+    let editing = this.items[index].item;
+    this.itemForm.setValue({ item: editing });
+  }
+
+  close() {
+    this.itemIndex = null;
+    this.itemForm.reset();
+  }
 }
